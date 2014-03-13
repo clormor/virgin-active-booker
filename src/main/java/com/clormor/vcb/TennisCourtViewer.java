54289cd2360b36c745de82367630d3bf7da2dccf
clormor <clormor@palantir.com>
2014-03-13 10:11:05 +0000

    initial commit

---
+++ src/main/java/com/clormor/vcb/TennisCourtViewer.java
@@ -0,0 +1,100 @@
+package com.clormor.vcb;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class TennisCourtViewer {
+	
+	private final String username;
+	private final String password;
+
+	public TennisCourtViewer(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+	
+	public void printAvailableCourts() {
+		// Create a new instance of the Firefox driver
+		// Notice that the remainder of the code relies on the interface,
+		// not the implementation.
+		WebDriver driver = new FirefoxDriver();
+
+		driver.get("https://memberportal.esporta.com/");
+		WebElement usernameElement = waitForElement(driver,
+				By.name("edUsername"));
+		WebElement passwordElement = waitForElement(driver,
+				By.name("edPassword"));
+		WebElement loginButton = waitForElement(driver, By.name("btnOK"));
+
+		usernameElement.sendKeys(username);
+		passwordElement.sendKeys(password);
+		loginButton.click();
+
+		WebElement newBookingButton = waitForElement(driver, By.name("btnNB"));
+		newBookingButton.click();
+
+		WebElement listViewRadioButton = waitForElement(driver,
+				By.id("rbSearch"));
+		listViewRadioButton.click();
+
+		WebElement proceedStep2Button = waitForElement(driver,
+				By.id("rpGoStep2_b"));
+		proceedStep2Button.click();
+
+		WebElement day8RadioButton = waitForElement(driver, By.id("rbDay8"));
+		day8RadioButton.click();
+
+		WebElement proceedStep3Button = waitForElement(driver,
+				By.id("rpProceed_b"));
+		proceedStep3Button.click();
+
+		WebElement _6pmRadioButton = waitForElement(driver, By.id("rb_11_0"));
+		_6pmRadioButton.click();
+
+		Select availableCourtsSelect = new Select(waitForElement(driver,
+				By.id("alb_5")));
+
+		List<WebElement> availableCourtElements = availableCourtsSelect
+				.getOptions();
+		for (WebElement availableCourt : availableCourtElements) {
+			System.out.println(availableCourt.getText() + " is available");
+		}
+
+		driver.quit();
+	}
+
+	/**
+	 * Private method that acts as an arbiter of implicit timeouts of sorts..
+	 * sort of like a Wait For Ajax method.
+	 */
+	private static WebElement waitForElement(WebDriver driver, By by) {
+		int attempts = 0;
+		int size = driver.findElements(by).size();
+
+		while (size == 0) {
+			size = driver.findElements(by).size();
+			if (attempts == 3) {
+				throw new RuntimeException(String.format(
+						"Could not find %s after %d seconds", by.toString(), 3));
+			}
+			attempts++;
+			try {
+				Thread.sleep(1000); // sleep for 1 second.
+			} catch (Exception x) {
+				throw new RuntimeException(
+						"Failed due to an exception during Thread.sleep!", x);
+			}
+		}
+
+		if (size > 1)
+			System.err.println("WARN: There are more than 1 " + by.toString()
+					+ " 's!");
+
+		return driver.findElement(by);
+	}
+}
---
+++ src/main/java/com/clormor/vcb/VirginActiveCli.java
@@ -0,0 +1,66 @@
+package com.clormor.vcb;
+
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+public class VirginActiveCli {
+
+	public static void main(String[] args) {
+		new VirginActiveCli(args);
+	}
+	
+	final HelpFormatter formatter;
+	final Options options;
+
+	public VirginActiveCli(String[] args) {
+		formatter = new HelpFormatter();
+		options = new Options();
+		options.addOption("p", true, "your member's portal password");
+		options.addOption("u", true, "your member's portal username");
+		options.addOption("view", false, "list available sessions/courts");
+		options.addOption("book", false, "book a session/court");
+		options.addOption("help", false, "print this help message");
+		options.addOption("h", false, "print this help message");
+		
+		processArgs(args);
+	}
+
+	private void processArgs(String[] args) {
+		CommandLineParser parser = new BasicParser();
+		CommandLine cmd = null;
+		
+		try {
+			cmd = parser.parse(options, args);
+		} catch (ParseException e) {
+			printHelpMessage();
+			System.exit(1);
+		}
+
+		if (! cmd.hasOption('p')) {
+			printHelpMessage();
+			System.exit(1);
+		}
+		
+		if (! cmd.hasOption('u')) {
+			printHelpMessage();
+			System.exit(1);
+		}
+		
+		if (cmd.hasOption("help") || cmd.hasOption('h')) {
+			formatter.printHelp("virginActiveCli", options);	
+		}
+		
+		String username = cmd.getOptionValue('u');
+		String password = cmd.getOptionValue('p');
+		TennisCourtViewer tcv = new TennisCourtViewer(username, password);
+		tcv.printAvailableCourts();
+	}
+	
+	private void printHelpMessage() {
+		formatter.printHelp("virginActive", options);
+	}
+}
