a79de65b84c27c134590a86f5cb53aaac4e25006
clormor <clormor@palantir.com>
2014-03-20 07:50:32 -0400

    refactored packages and started testing the CLI - added test to ensure cannot specify book and list

---
+++ src/main/java/com/clormor/vab/client/VirginActiveClient.java
@@ -0,0 +1,11 @@
+package com.clormor.vab.client;
+
+import org.apache.commons.cli.ParseException;
+
+public interface VirginActiveClient {
+
+	void processArgs(String[] args) throws ParseException;
+	
+	void run();
+	
+}
---
+++ src/test/java/com/clormor/vab/client/VirginActiveClientTest.java
@@ -0,0 +1,30 @@
+package com.clormor.vab.client;
+
+import org.apache.commons.cli.ParseException;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.clormor.vab.client.VirginActiveClientImpl;
+
+public class VirginActiveClientTest {
+
+	private VirginActiveClientImpl testCli;
+	
+	@Before
+	public void setup() {
+		testCli = new VirginActiveClientImpl();
+	}
+	
+	@Test (expected = ParseException.class)
+	public void parseNoArgs() throws ParseException {
+		String[] args = {""};
+		testCli.processArgs(args);
+	}
+	
+	@Test (expected = ParseException.class)
+	public void parseViewAndList() throws ParseException {
+		String[] args = {"-u", "me", "-p", "whatever", "-list", "-book"};
+		testCli.processArgs(args);
+	}
+	
+}
